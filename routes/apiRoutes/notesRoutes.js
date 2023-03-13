7938103dddcffd00b27453b8367ff1a3b7e772a3
EllieWalsch <elliewalsch4@gmail.com>
2023-03-13 15:17:59 -0500

    Add a new note

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,8 +1,33 @@
 const notes = require('express').Router();
-const { readFromFile } = require('../../helpers/fsUtils.js');
+const { readFromFile, readAndAppend } = require('../../helpers/fsUtils.js');
 
 notes.get('/', (req, res) =>
   readFromFile('C:/Users/ellie/code/note-taker2/db/db.json').then((data) => res.json(JSON.parse(data)))
 );
+
+notes.post('/', (req, res) => {
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+      };
+  
+      readAndAppend(newNote, 'C:/Users/ellie/code/note-taker2/db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in adding note');
+    }
+  });
 
 module.exports = notes;
