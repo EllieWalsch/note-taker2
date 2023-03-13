4d1f871f54820e5709746a047dfad52b9953b495
EllieWalsch <elliewalsch4@gmail.com>
2023-03-13 14:40:56 -0500

    Use html routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const htmlRoutes = require('./routes/htmlRoutes/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,17 +9,9 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
